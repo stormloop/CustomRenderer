18a83ebbce6e1c0456af90ce23711d840a3df878
stormloop <storm.vandewijer@gmail.com>
2023-07-10 22:13:54 +0200

    histogram coloring

--- Assets/Scripts/FractalCameraScript.cs
+++ Assets/Scripts/FractalCameraScript.cs
@@ -45,7 +45,13 @@ public class FractalCameraScript : MonoBehaviour
         {
             Destroy(tex);
 
-            tex = new RenderTexture(Screen.width, Screen.height, 24);
+            tex = new RenderTexture(
+                Screen.width,
+                Screen.height,
+                24,
+                RenderTextureFormat.ARGBFloat,
+                RenderTextureReadWrite.Linear
+            );
             tex.enableRandomWrite = true;
             tex.Create();
             bl = false;
@@ -58,7 +64,21 @@ public class FractalCameraScript : MonoBehaviour
             lastBL = BL;
             lastTR = TR;
 
-            shader.SetTexture(shader.FindKernel("Render"), "Result", tex);
+            // shader.SetTexture(shader.FindKernel("Render"), "Result", tex);
+            // shader.SetFloat("DeltaTime", Time.deltaTime);
+            // shader.SetFloats("BL", BL.x, BL.y);
+            // shader.SetFloats("TR", TR.x, TR.y);
+            // shader.SetFloats("InputC", InputC.x, InputC.y);
+            // shader.SetBool("Julia", Julia);
+            // shader.SetInt("Margin", 50);
+            // shader.SetInt("Depth", Depth);
+            // shader.SetInts("Resolution", tex.width, tex.height);
+            // shader.Dispatch(shader.FindKernel("Render"), tex.width / 7, tex.height / 7, 1);
+
+            ComputeBuffer buffer = new ComputeBuffer(Depth, sizeof(int));
+            buffer.SetData(new int[Depth]);
+            shader.SetBuffer(shader.FindKernel("GenerateHistogram"), "IterationDepths", buffer);
+            shader.SetTexture(shader.FindKernel("GenerateHistogram"), "Result", tex);
             shader.SetFloat("DeltaTime", Time.deltaTime);
             shader.SetFloats("BL", BL.x, BL.y);
             shader.SetFloats("TR", TR.x, TR.y);
@@ -67,7 +87,13 @@ public class FractalCameraScript : MonoBehaviour
             shader.SetInt("Margin", 50);
             shader.SetInt("Depth", Depth);
             shader.SetInts("Resolution", tex.width, tex.height);
-            shader.Dispatch(shader.FindKernel("Render"), tex.width / 7, tex.height / 7, 1);
+            shader.Dispatch(shader.FindKernel("GenerateHistogram"), tex.width / 7, tex.height / 7, 1);
+
+            shader.SetBuffer(shader.FindKernel("RenderHistogram"), "IterationDepths", buffer);
+            shader.SetTexture(shader.FindKernel("RenderHistogram"), "Result", tex);
+            shader.Dispatch(shader.FindKernel("RenderHistogram"), tex.width / 7, tex.height / 7, 1);
+            
+            buffer.Dispose();
         }
 
         Graphics.Blit(tex, dest);
